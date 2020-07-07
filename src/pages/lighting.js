8e4c53739dfca49d2e9106bbf23d00cbe761ea3d
eosti <reidsox1@gmail.com>
2020-07-06 19:39:43 -0700

    switch lighting to grid

--- src/pages/lighting.js
+++ src/pages/lighting.js
@@ -10,23 +10,19 @@ export default ({ data }) => {
     return (
         <Layout>
             <SEO title="Lighting Design" />
-            <div>
-                <ul class="mr-4">
+            <div class="grid grid-cols-1 gap-4 px-4 my-4">
                 {data.allMarkdownRemark.edges.map(({ node }) => (
-                    <div key={node.id} class="show-box relative overflow-hidden my-0 mx-auto mb-3">
-                        <li class="h-full">
+                    <div key={node.id} class="overflow-hidden h-full show-box">
                             <Link class="h-full" to={node.fields.slug}>
                                 <div class="h-full object-center">
                                     <Img style={{height:'100%'}} imgStyle={{ objectPosition: 'center' }} fluid={node.frontmatter.featuredImage.childImageSharp.fluid} />
                                 </div>
-                                <div class="h-full w-full absolute top-0 opacity-0 hover:opacity-75 duration-300 bg-white">
+                                <div class="h-full w-full left-0 top-0 relative opacity-0 hover:opacity-75 duration-300 bg-white show-name">
                                     <h1 class="show-title">{node.frontmatter.title}</h1>
                                 </div>
                             </Link>
-                        </li>
                     </div>
                 ))}
-                </ul>
             </div>
         </Layout>
     )
