090031852c365ead4c0888acf995e12c9fe60839
eosti <reidsox1@gmail.com>
2020-12-04 16:23:38 -0800

    add linkedin to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,8 @@ const Footer = () => {
         <div class="bottom-0">
             <div class="flex flex-row content-center justify-center pb-2">
                 <a class="pr-8 text-gray-800" href="mailto:hello@reidsoxharris.me"><i class="fas fa-envelope"></i></a>
-                <a class="text-gray-800" target="_blank" rel="noopener noreferrer" alt="GitHub Profile" href="https://github.com/eosti"><i class="fab fa-github"></i></a>
+                <a class="pr-8 text-gray-800" target="_blank" rel="noopener noreferrer" alt="GitHub Profile" href="https://github.com/eosti"><i class="fab fa-github"></i></a>
+                <a class="text-gray-800" target="_blank" rel="noopener noreferrer" alt="LinkedIn Profile" href="https://www.linkedin.com/in/reid-sox-harris/"><i class="fab fa-linkedin"></i></a>
             </div>
             <p class="text-gray-600 text-light text-sm mb-2">© {today.getFullYear()} Reid Sox-Harris.</p>
         </div>
