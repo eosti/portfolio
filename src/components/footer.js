7e43baa146be93f91f521a2e9843e13d1cc1f179
eosti <reidsox1@gmail.com>
2021-06-18 22:11:51 -0700

    move location of alt tags

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
     return (
         <div class="bottom-0">
             <div class="flex flex-row content-center justify-center pb-2">
-                <a class="pr-8 text-gray-800" href="mailto:hello@reidsoxharris.me"><i class="fas fa-envelope"></i></a>
-                <a class="pr-8 text-gray-800" target="_blank" rel="noopener noreferrer" alt="GitHub Profile" href="https://github.com/eosti"><i class="fab fa-github"></i></a>
-                <a class="text-gray-800" target="_blank" rel="noopener noreferrer" alt="LinkedIn Profile" href="https://www.linkedin.com/in/reid-sox-harris/"><i class="fab fa-linkedin"></i></a>
+                <a class="pr-8 text-gray-800" href="mailto:hello@reidsoxharris.me"><i class="fas fa-envelope" alt="Email"></i></a>
+                <a class="pr-8 text-gray-800" target="_blank" rel="noopener noreferrer" Profile" href="https://github.com/eosti"><i class="fab fa-github" alt="GitHub Profile"></i></a>
+                <a class="text-gray-800" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/reid-sox-harris/"><i class="fab fa-linkedin" alt="LinkedIn Profile"></i></a>
             </div>
             <p class="text-gray-600 text-light text-sm mb-2 pb-2">© {today.getFullYear()} Reid Sox-Harris.</p>
         </div>
