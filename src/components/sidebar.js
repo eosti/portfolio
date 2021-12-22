4e95bf5f76097f4bfaf4046d1b0c4ece1800ed83
eosti <reidsox1@gmail.com>
2021-12-21 20:46:33 -0800

    migrate to staticImage

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image";
 
-import "./sidebar.css"
-
 export default ({ children }) => {
     const data = useStaticQuery(
         graphql`
@@ -18,34 +16,34 @@ export default ({ children }) => {
     )
 
     return (
-        <aside class="flex items-center justify-center max-h-screen w-1/6 max-w-xs">
-            <nav class="inline-block fixed align-middle wrap-normal">
-                <Link to="/">
-                    <StaticImage src="../images/reid-logo.png" alt="Logo" loading="eager" imgClassName="logo-image h-auto justify-center pb-2" />
+        <aside class="flex items-center justify-center max-h-screen w-1/6">
+            <nav class="flex flex-col justify-center items-start wrap-normal w-max">
+                <Link to="/" className="flex-auto w-max mb-8 mt-4">
+                    <StaticImage className="w-28" placeholder="tracedSVG" loading="eager" alt="Logo" src="../images/reid-logo.png" />
                 </Link>
-                <ul class="list-none p-0">
+                <ul class="flex-auto list-none p-0 m-0 w-max">
                     <li>
-                        <Link to="/" className="text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700">
+                        <Link to="/" className="text-lg text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700">
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link to="/about/" className="text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700">
+                        <Link to="/about/" className="text-lg text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700">
                             About
                         </Link>
                     </li>
                     <li>
-                        <Link to="/lighting/" className="text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700" partiallyActive={true}>
+                        <Link to="/lighting/" className="text-lg text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700" partiallyActive={true}>
                             Lighting Design
                         </Link>
                     </li>
                     <li>
-                        <Link to="/projects/" className="text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700" partiallyActive={true}>
+                        <Link to="/projects/" className="text-lg text-gray-900 hover:text-purple-700 font-light" activeClassName="font-semibold text-gray-900 hover:text-purple-700" partiallyActive={true}>
                             Projects
                         </Link>
                     </li>
                     <li>
-                        <a href="mailto:hello@reidsoxharris.me" class="text-gray-900 hover:text-purple-700 font-light">
+                        <a href="mailto:hello@reidsoxharris.me" class="text-lg text-gray-900 hover:text-purple-700 font-light">
                             Contact
                         </a>
                     </li>
