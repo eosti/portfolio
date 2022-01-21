5cb337be646f6c8b366ce50cfa5dfd300b9f8305
eosti <reidsox1@gmail.com>
2022-01-20 23:49:10 -0500

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,50 +6,63 @@ function AboutPage() {
     return (
         <>
             <SEO title="About" />
-            <div className="flex flex-col text-left align-center m-auto lg:min-h-full mx-16 lg:mx-32 lg:h-screen mt-0 lg:mt-16">
-                <div className="text-6xl font-thin pt-4 lg:pt-12 pb-8 pl-0 text-purple-600">
-                    About
-                </div>
-                <h4 className="mb-4">Electronics</h4>
-                <ul>
-                    <li>
-                        Second-year electrical/computer engineer at University
-                        of Toronto
-                    </li>
-                    <li>
-                        Programming Experience: C/C++, Python, bash, git,
-                        MATLAB, JavaScript
-                    </li>
-                    <li>
-                        Embedded controllers: Arduino, MSP430, STM32, Raspberry
-                        Pi
-                    </li>
-                </ul>
-                <h4 className="mb-4">Lighting</h4>
-                <ul>
-                    <li>
-                        Designed for companies such as A Theatre Near U and Paly
-                        Theatre
-                    </li>
-                    <li>
-                        Electrician for Theatreworks Silicon Valley, West Bay
-                        Opera, and others
-                    </li>
-                </ul>
-                <h4 className="mb-4">Other</h4>
-                <ul>
-                    <li>
-                        Unhealthy addiction to keyboards
+            <div className="flex flex-col text-left m-auto mx-8 lg:mx-32 h-full lg:min-h-screen mt-0 lg:pt-8 w-100">
+                <div className="max-w-max 2xl:w-1/2 md:w-3/4 w-full m-auto">
+                    <div className="text-6xl w-max font-thin pt-4 lg:pt-0 pb-8 pl-0 text-purple-600">
+                        About
+                    </div>
+                    <div className="w-full">
+                        <div className="pb-8 w-full">
+                            Hey! I&apos;m an electrical engineering student with
+                            plenty of curiosity and a love of learning. I&apos;m
+                            currently a board designer with the University of
+                            Toronto Aerospace Team, and have a long list of side
+                            projects that I&apos;m persuing. Here&apos;s a bit
+                            more about me:
+                        </div>
+                        <h4 className="mb-4 w-full">Electronics</h4>
                         <ul>
                             <li>
-                                Current daily: Iris Rev3 with custom silent
-                                Outemu Ice 68g and GMK Oblivion v2
+                                Second-year electrical engineer at University of
+                                Toronto
+                            </li>
+                            <li>
+                                Programming Experience: C/C++, Python, bash,
+                                git, Verilog HDL, MATLAB
+                            </li>
+                            <li>
+                                Embedded controllers: Arduino, MSP430, STM32,
+                                Raspberry Pi
                             </li>
                         </ul>
-                    </li>
-                    <li>(neo)vim {">>"} emacs</li>
-                    <li>Trains are cool. So are airplanes.</li>
-                </ul>
+                        <h4 className="mb-4 w-full">Lighting</h4>
+                        <ul>
+                            <li>
+                                Designed for companies such as A Theatre Near U
+                                and Paly Theatre
+                            </li>
+                            <li>
+                                Electrician for Theatreworks Silicon Valley,
+                                West Bay Opera, and others
+                            </li>
+                        </ul>
+                        <h4 className="mb-4 w-full">Other</h4>
+                        <ul>
+                            <li>
+                                Unhealthy addiction to keyboards
+                                <ul>
+                                    <li>
+                                        Current daily: Iris Rev3 with custom
+                                        silent Outemu Ice 68g and GMK Oblivion
+                                        v2
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>(neo)vim {">>"} emacs</li>
+                            <li>Trains are cool. So are airplanes.</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </>
     )
